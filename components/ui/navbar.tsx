d05a0e0170d0ede0123b3057157e0b1c5d08d2ab
Harshil Rakeshkumar Varia <hr855863@dal.ca>
2025-05-31 21:24:28 -0300

    clerk auth dependencies installed and singin implemented

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -1,25 +1,34 @@
 import Link from "next/link";
 import Image from "next/image";
-import Navitems from "./NavItems";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import NavItems from "./NavItems";
 
-const Navbar= () => {
-    return(
-       <nav className="navbar">
+const Navbar = () => {
+    return (
+        <nav className="navbar">
             <Link href="/">
-                <div className="flex item-center gap-2.5 cursor-pointer">
-                    <Image src="/images/logo.svg" alt={""} height={44} width={46}                  
+                <div className="flex items-center gap-2.5 cursor-pointer">
+                    <Image
+                        src="/images/logo.svg"
+                        alt="logo"
+                        width={46}
+                        height={44}
                     />
-
                 </div>
             </Link>
-            <div className="flex item-center gap-8">
-                <Navitems/>
-                <p>Sign-In</p>
-
+            <div className="flex items-center gap-8">
+                <NavItems />
+                <SignedOut>
+                    <SignInButton>
+                        <button className="btn-signin">Sign In</button>
+                    </SignInButton>
+                </SignedOut>
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
             </div>
-
-       </nav>
+        </nav>
     )
 }
 
-export default Navbar;
+export default Navbar
